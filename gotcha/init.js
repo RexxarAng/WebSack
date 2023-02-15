297ac31b7b406c6ec76d593b3227e0be41065c8d
YeongJK98 <yeongjingkang@hotmail.com>
2023-02-15 22:45:43 +0800

    test run mouse drawing canvas

--- gotcha/init.js
+++ gotcha/init.js
@@ -1,5 +1,73 @@
 let captcha = new Array();
 
+
+/**
+ * 
+ * @param {*} cw : Number specifying canvas width
+ * @param {*} ch : Number specifyng canvas height
+ */
+function initCanvas(cw, ch) {
+  // Create a canvas element and set its width and height
+  const canvas = document.createElement('canvas');
+  canvas.width = cw;
+  canvas.height = ch;
+
+  // Get the 2D context of the canvas
+  const context = canvas.getContext('2d');
+
+  // Set the initial drawing state
+  context.strokeStyle = 'black';
+  context.lineWidth = 2;
+
+  // Keep track of the mouse position and drawing state
+  let isDrawing = false;
+  let lastMouse = { x: 0, y: 0 };
+
+  // Add the canvas element to the page
+  document.body.appendChild(canvas);
+
+  // Add event listeners for mouse movement
+  canvas.addEventListener('click', function(event) {
+    if (event.button === 0) { // check for left mouse button
+      isDrawing = true;
+      lastMouse = { x: event.clientX, y: event.clientY };
+    }
+  });
+  
+  canvas.addEventListener('mousemove', function(event) {
+    if (isDrawing) {
+      const currentMouse = { x: event.clientX, y: event.clientY };
+      context.beginPath();
+      context.moveTo(lastMouse.x, lastMouse.y);
+      context.lineTo(currentMouse.x, currentMouse.y);
+      context.stroke();
+      lastMouse = currentMouse;
+    }
+  });
+  // Release mouse click
+  canvas.addEventListener('mouseup', function(event) {
+    if (event.button === 0) { // check for left mouse button
+      isDrawing = false;
+    }
+  });
+  // Leaves canvas area
+  canvas.addEventListener('mouseleave', function(event) {
+    if (event.button === 0) { // check for left mouse button
+      isDrawing = false;
+    }
+  });
+
+  // Clear the canvas when the user clicks the right mouse button
+  canvas.addEventListener('mousedown', function(event) {
+    if (event.button === 2) {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+    }
+  });
+}
+
+
+
+
 function createCaptcha() {
     const activeCaptcha = document.getElementById("captcha");
     for (q = 0; q < 6; q++) {
