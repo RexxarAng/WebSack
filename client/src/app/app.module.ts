91fd672aaaecbf45e667b64054903e5320e21068
RexxarAng <rexar.aa@gmail.com>
2023-02-19 17:40:32 +0000

    Added Login Page UI

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { NgbModule, NgbToastModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,9 @@ import { SignupComponent } from './signup/signup.component';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NgbToastModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
+  loginError = false;
+  signInData: any = {};
 
+
+  onSubmit(form: NgForm) {
+
+
+    if (form.valid) {
+      this.loginError = false
+      this.signInData = {
+        email: this.email,
+        password: this.password
+      };
+      console.log('Form submitted!');
+      console.log(this.signInData);
+
+      if (this.email !== 'test' || this.password !== '12345') {
+        this.loginError = true;
+      } else {
+        // If the login succeeds, we'll clear the error message and
+        // navigate to the user's dashboard.
+        this.loginError = false;
+        // Use your preferred routing method here, such as Angular Router or window.location.
+        window.location.href = '/dashboard';
+      }
+    } else {
+      this.loginError = true
+    }
+  }
 }
